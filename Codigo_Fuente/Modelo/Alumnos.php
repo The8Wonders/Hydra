86296a5ee9d0a806320e1072453f20fe84c09a9e
Matias <matias.moreno1701@alumnos.ubiobio.cl>
2019-10-16 16:27:50 -0300

    Creacion carpetas de estructura

---
+++ Codigo_Fuente/Modelo/Alumnos.php
@@ -0,0 +1,26 @@
+<?php
+
+
+  class alumnos{
+    public function registrarAlumno($datos){
+
+    }
+
+    public function loginAlumno($datos){
+
+    }
+
+    public function obtenerdatosAlumno($idalumno){
+
+    }
+
+    public function eliminarAlumno($idalumno){
+
+    }
+
+    public function actualizaAlumno($datos){
+      
+    }
+  }
+
+?>
---
+++ Codigo_Fuente/Modelo/Profesores.php
@@ -0,0 +1,26 @@
+<?php
+
+
+  class profesor{
+    public function registrarProfesor($datos){
+
+    }
+
+    public function loginProfesor($datos){
+
+    }
+
+    public function obtenerdatosProfesor($idalumno){
+
+    }
+
+    public function eliminarProfesor($idalumno){
+
+    }
+
+    public function actualizaProfesor($datos){
+      
+    }
+  }
+
+?>
