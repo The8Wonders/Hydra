2e52bc4a5ef38f5d9f1e89dd03879968bb4e4fe7
Matias <maatiigmm@gmail.com>
2020-03-13 15:25:13 -0300

    Login Alumno Alertas

--- Codigo_Fuente/Modelo/login.modelo.php
+++ Codigo_Fuente/Modelo/login.modelo.php
@@ -1,15 +1,14 @@
-<?php   
-    if($peticionAjax){
-      require_once "../core/mainModel.php";
-    }else{
-      require_once "./core/mainModel.php";
-    }
-    class loginmodelo extends mainModel{
-        protected function ingresar_modelo($datos){
-            $sql = mainModel::conectar()->prepare("SELECT * FROM usuario WHERE rut= :Rut AND contraseña= :Clave");
-            $sql->bindParam(":Rut", $datos['Rut']);
-            $sql->bindParam(":Clave", $datos['Clave']);
-            $sql->execute();
-            return $sql;
-        }
-    }
+<?php
+
+require_once "../core/mainModel.php";
+class loginmodelo extends mainModel
+{
+  protected function ingresar_modelo($datos)
+  {
+    $sql = mainModel::conectar()->prepare("SELECT * FROM usuario WHERE rut= :Rut AND contraseña= :Clave");
+    $sql->bindParam(":Rut", $datos['Rut']);
+    $sql->bindParam(":Clave", $datos['Clave']);
+    $sql->execute();
+    return $sql;
+  }
+}
---
+++ Codigo_Fuente/vistas/assets/js/formularios.js
@@ -0,0 +1,113 @@
+console.log('funcionando');
+
+var formulario = document.getElementById('formAlumno');
+
+formulario.addEventListener('submit', function (e) {
+	e.preventDefault();
+	console.log('Me diste un click')
+
+	var datos = new FormData(formulario);
+
+	console.log(datos)
+	console.log(datos.get('rut'))
+	console.log(datos.get('contra'))
+
+	fetch('../../../controladores/alumno.controlador.php', {
+		method: 'POST',
+		body: datos
+	})
+		.then(res => res.json())
+		.then(data => {
+			console.log(data)
+
+			if (data == 'incompletos') {
+				Swal.fire({
+					icon: 'error',
+					title: 'Lo sentimos',
+					text: 'Debe rellenar todo los campos',
+				})
+			} else {
+				if (data == 'correcto') {
+					window.location = "../vistas/contenidos/home-vistas.php";
+				} else {
+					if (data == 'contraseñas') {
+
+						Swal.fire({
+							icon: 'error',
+							title: 'Lo sentimos',
+							text: 'Las contraseñas no coinciden',
+						})
+					} else {
+						if (data == 'incorrecto') {
+							Swal.fire({
+								icon: 'error',
+								title: 'Lo sentimos',
+								text: 'No se pudo registrar la cuenta',
+							})
+						} else {
+							if (data == 'rut') {
+								Swal.fire({
+									icon: 'error',
+									title: 'Lo sentimos',
+									text: 'El rut ya se encuentra registrado',
+								})
+							} else {
+								if (data == 'correo') {
+									Swal.fire({
+										icon: 'error',
+										title: 'Lo sentimos',
+										text: 'El correo ya se encuentra registrado',
+									})
+								}
+							}
+						}
+					}
+
+				}
+			}
+
+		})
+})
+
+
+var formularioLogin = document.getElementById('formLogin');
+
+formularioLogin.addEventListener('submit', function (e) {
+	e.preventDefault();
+	console.log('Haz echo click');
+
+	var datos = new FormData(formularioLogin);
+
+	console.log(datos)
+	console.log(datos.get('rut_usuario'))
+	console.log(datos.get('contra'))
+
+	fetch('../../../controladores/login.controlador.php', {
+		method: 'POST',
+		body: datos
+	})
+		.then(res => res.json())
+		.then(data => {
+			console.log(data)
+
+			if (data == 'incompletos') {
+				Swal.fire({
+					icon: 'error',
+					title: 'Lo sentimos',
+					text: 'Debe rellenar todo los campos',
+				})
+			} else {
+				if (data == 'noexiste') {
+					Swal.fire({
+						icon: 'error',
+						title: 'Vuelva a intentarlo',
+						text: 'Error en el rut o la contraseña',
+					})
+				} else {
+					if (data == 'existe') {
+						window.location = "../vistas/contenidos/home-vistas.php";
+					}
+				}
+			}
+		})
+})
