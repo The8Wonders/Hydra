2acb0bca6bada5e6d85defd28feaf403a67c755d
Matias <maatiigmm@gmail.com>
2020-03-27 13:05:29 -0300

    Eliminar Alumno

--- Codigo_Fuente/controladores/alumno.controlador-eliminar.php
+++ Codigo_Fuente/controladores/alumno.controlador-eliminar.php
@@ -6,10 +6,18 @@
        
         $id = $_GET['cod'];
         $res = mainModel::ejecutar_consulta_simple("DELETE FROM alumno WHERE rut='$id'");
-           
+        if($res->rowCount()>=1){
+          $res2 = mainModel::eliminar_cuenta($id);
+
+          if($res2->rowCount()>=1){
+            header("Location:../vistas/contenidos/tableAlumnos-vistas.php");
+          }else{
+            $res3 = mainModel::ejecutar_consulta_simple("INSERT INTO alumno (rut) VALUES rut='$id'");
+          }
+        }
     }
 
 
-   header("Location:../vistas/contenidos/tableAlumnos-vistas.php");
+   
 
 ?>
