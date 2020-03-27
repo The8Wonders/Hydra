4106e35220ca4001fbc8a030f98ef90a4a7397c0
laiger290 <nicolasblur@gmail.com>
2020-03-27 03:29:38 -0300

    FINAL NICOLAS VASQUEZ

--- Codigo_Fuente/controladores/update.admin.alumno.controlador2.php
+++ Codigo_Fuente/controladores/update.admin.alumno.controlador2.php
@@ -1,80 +1,48 @@
 <?php
-//require_once "../modelo/profesor.modelo.php";
-require_once "../core/mainModel.php";
-class update_adminalumno extends alumnomodelo{
-    public function update_adminalumno_controlador(){
 
-    $rut = mainModel::limpiar_cadena($_POST['rut']);
+require_once "../modelo/alumno.modelo.php";
+
+class update_adminalumno extends alumnomodelo
+{
+
+  public function update_adminalumno_controlador()
+  {
+    $rutR = mainModel::limpiar_cadena($_POST['rut']);
     $nombre = mainModel::limpiar_cadena($_POST['nombre']);
     $apellido = mainModel::limpiar_cadena($_POST['apellido']);
     $telefono = mainModel::limpiar_cadena($_POST['telefono']);
     $correo = mainModel::limpiar_cadena($_POST['correo']);
-    $rol = mainModel::limpiar_cadena($_POST['rol']);
-    $cargo = mainModel::limpiar_cadena($_POST['cargo']);
-    $equipo = mainModel::limpiar_cadena($_POST['equipo']);
 
-    if ($rut == "" || $nombre == "" || $apellido == "" || $correo == "" || $rol == "" || $telefono == "" 
-    || $cargo == "" || $equipo == "") {
+    if ($rutR == "" || $nombre == "" || $apellido == "" || $correo == "" ||  $telefono == "") {
       $respuesta = "incompletos";
-      
-      //header("Location:../vistas/contenidos/perfil-vistas.php");
     } else {
-        $consulta1 = mainModel::ejecutar_consulta_simple("SELECT rut FROM usuario WHERE rut= '$rut' AND cod_rol = 'alumno' ");
+        $consulta1 = mainModel::ejecutar_consulta_simple("SELECT rut FROM usuario WHERE rut= '$rutR' AND cod_rol = 'alumno' ");
 
         if($consulta1->rowCount()>=1){
           
           $editarCuenta = [
-            "Rut" => $rut,
+            "Rut" => $rutR,
             "Nombre" => $nombre,
             "Apellido" => $apellido,
             "Telefono" => $telefono,
-            "Correo" => $correo,
-            "Rol" => $rol
+            "Correo" => $correo
           ];
 
+          $actualixar = alumnomodelo::actualizar_alumno_modelo2($editarCuenta);
 
-          $consulta2 = mainModel::ejecutar_consulta_simple("SELECT rut FROM alumno WHERE rut= '$rut' ");
-
-        if($consulta2->rowCount()>=1){
-          
-          $editarCuenta2 = [
-            "Rut" => $rut,
-            "Cargo" => $cargo,
-            "Equipo" => $equipo
-          ];
-
-          //$actualizarRut= profesormodelo::update_rut_profesor($rut);
-
-          //if($actualizarRut->rowCount()>=1){
-            
-            $actualixar = alumnomodelo::actualizar_alumno_modelo2($editarCuenta);
-    
-            if($actualixar->rowCount()>=1){
-              //header("Location:../vistas/contenidos/tableAlumnos-vistas.php");
-    
-              $actualixar2 = alumnomodelo::actualizar_alumno_admin_modelo($editarCuenta2);
-              if($actualixar2->rowCount()>=1){
-                $respuesta = "Actualizada";
-              }
-            
-            }else{
-              $respuesta = "Error";
-              //header("Location:../vistas/contenidos/perfil-vistas.php");
-            }
-
-          //}
+          if($actualixar->rowCount()>=1){
+            $respuesta = "Actualizada";
+          }else{
+            $respuesta = "Error";
+          }
 
         }else{
-          $respuesta = "NoencuentraAlumno";
-          //header("Location:../vistas/contenidos/perfil-vistas.php");
+          $respuesta = "NoencuentraProfesor";
         }
     }
 
     return $respuesta;
-    //header("Location:../vistas/contenidos/perfil-vistas.php");
   }
 
   
 }
-
-?>
