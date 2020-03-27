0f97ee8d05c448078de6dff7bb8c17fc2a217b41
Matias <maatiigmm@gmail.com>
2020-03-26 22:44:32 -0300

    aps

--- Codigo_Fuente/vistas/contenidos/tableProfesor-vistas.php
+++ Codigo_Fuente/vistas/contenidos/tableProfesor-vistas.php
@@ -55,7 +55,6 @@
                         <td><?php echo $rows['telefono'] ?></td>
                         <?php echo "<td><a href='editProfesor-vistas.php?rut=" . $rows['rut'] . "'><i class='far fa-edit'></i></a></td>" ?>
                         <?php echo "<td><a href='../../controladores/eliminar.profesor.controlador.php?rut=" . $rows['rut'] . "'><i class='fas fa-times'></i></a></td>" ?>
-                        
                       </tr>
                     <?php } ?>
                   </tbody>
